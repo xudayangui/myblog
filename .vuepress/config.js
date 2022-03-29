c8b1b82f12453a7a992773127956f214dd993474
addis <addis@itcom888.com>
2022-03-29 19:08:53 +0800

    修改主题

---
+++ .vuepress/config.js
@@ -0,0 +1,80 @@
+const {getChildren} = require("./atuo")
+module.exports = {
+  "title": "myBlog",
+  "description": "   ",
+  "locales": {
+    '/': {
+      "lang": 'zh-CN'
+    }
+  },
+  "dest": "dist",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "主页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间线",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "github",
+        "icon": "reco-message",
+        "link": "https://github.com/recoluan",
+      }
+    ],
+    "subSidebar": 'auto',//在所有页面中启用自动生成右侧边栏，原 sidebar 仍然兼容
+    "sidebar": {
+      // "/docs/theme-reco/": getChildren('./docs/theme-reco/'),
+      // [
+      //   "",
+      //   "theme",
+      //   "plugin",
+      //   "api"
+      // ],
+      "/docs/javascript/":  getChildren('./docs/javascript/'),
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "Dayangui",
+    "authorAvatar": "/avatar.jpg",
+    // "record": "xxxx",
+    "startYear": "2017"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
--- docs/README.md
+++
@@ -1,9 +0,0 @@
----
-home: true
-lang: zh-CN
-heroImage: /logo.jpg
-heroText: Dayangui的技术笔记
-tagline: 简洁至上
-actionText: 进入笔记 →
-actionLink: /notes/javascript/手动实现Ajax
----
